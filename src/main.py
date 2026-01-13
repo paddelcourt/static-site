5c29db7e52310c8863426b35cc8272bc6b14fa52
Tom <106786677+paddelcourt@users.noreply.github.com>
2026-01-13 12:57:09 +0100

    remove public

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from website import static_to_public, generate_pages_recursive
+import sys
 
 
 def main():
     static_to_public("static", "public")
-    generate_pages_recursive("content", "template.html","public")
-
+    basepath = sys.argv
+    generate_pages_recursive(f"{basepath}/content", "template.html",f"{basepath}/public")
+    
 
 main()
