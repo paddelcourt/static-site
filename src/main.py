251a913918a729eda477a0578dfa7343637dfbe0
Tom <106786677+paddelcourt@users.noreply.github.com>
2026-01-13 13:12:42 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from website import static_to_public, generate_pages_recursive
 
 
 def main():
-    static_to_public("static", "public")
+    static_to_public("static", "docs")
     generate_pages_recursive("content", "template.html","public")
     
 
